9b39430a44243c053004eceefd08042079727980
Dwight Doane <dwightdoane@me.com>
2020-01-02 03:08:16 -0500

    ux for fetching title

--- src/Page/MyMods.elm
+++ src/Page/MyMods.elm
@@ -9,7 +9,9 @@ import Http
 import Json.Decode as Decode
 import Json.Encode as Encode exposing (Value)
 import Network.Scraper as Scraper
+import Process
 import RemoteData exposing (RemoteData(..), WebData)
+import Task
 import Url
 
 
@@ -67,6 +69,10 @@ initialModel session =
 view model =
     Html.div []
         [ Html.div []
+            [ Html.p [] [ Html.text "My car" ]
+            , Html.text "2018 Subaru WRX Premium"
+            ]
+        , Html.div []
             [ Html.p [ Attributes.class "capitalize text-center" ]
                 [ Html.text "my mods" ]
             ]
@@ -121,44 +127,47 @@ viewNewLink k section =
             , Html.input
                 [ Attributes.class "border ml-2 flex-1 px-1 rounded-sm"
                 , Attributes.placeholder "https://fastcars.com"
-                , Events.onInput SetNewUrl
+                , Events.onInput <| SetNewUrl k
+                , Attributes.value section.newUrl
                 ]
                 []
             ]
-        , Html.div [ Attributes.class "flex items-center mb-1" ]
-            [ Html.label [ Attributes.class "font-medium text-sm" ] [ Html.text "Title:" ]
-            , Html.input
-                [ Attributes.class "border ml-2 flex-1 px-1 rounded-sm"
-                , Attributes.placeholder "Go fast parts"
-                , Events.onInput SetNewTitle
-                , Attributes.value section.newTitle
+        , Html.div [ Attributes.class " mb-1" ]
+            [ Html.div [ Attributes.class "flex items-center" ]
+                [ Html.label [ Attributes.class "font-medium text-sm" ]
+                    [ Html.text "Title:" ]
+                , Html.input
+                    [ Attributes.class "border ml-2 flex-1 px-1 rounded-sm"
+                    , Attributes.placeholder "Go fast parts"
+                    , Events.onInput <| SetNewTitle k
+                    , Attributes.disabled (RemoteData.isNotAsked section.suggestedTitle)
+                    , section.suggestedTitle
+                        |> RemoteData.map Attributes.value
+                        |> RemoteData.withDefault (Attributes.name "")
+                    , Attributes.placeholder <|
+                        -- consider using label to show fetching
+                        if RemoteData.isLoading section.suggestedTitle then
+                            "Fetching title..."
+
+                        else
+                            ""
+                    ]
+                    []
                 ]
-                []
-            , case section.suggestedTitle of
-                Loading ->
-                    Html.text "..."
-
-                Success title ->
-                    Html.button
-                        [ Events.onClick <|
-                            UseSuggestedTitle title
-                        , Attributes.type_ "button"
-                        , Attributes.class "mr-1 text-sm "
-                        ]
-                        [ Html.text title ]
-
-                _ ->
-                    Html.text ""
             ]
         , Html.div []
-            [ Html.label [ Attributes.class "mb-1 font-semibold text-sm" ] [ Html.text "Description:" ]
+            [ Html.label [ Attributes.class "mb-1 font-semibold text-sm" ]
+                [ Html.text "Description:" ]
             , Html.textarea
                 [ Attributes.placeholder "For shoutouts and things"
                 , Attributes.class "border rounded-sm w-full px-2 py-1"
                 ]
                 []
             ]
-        , Html.button [ Attributes.class "px-4 py-2 font-medium text-center rounded-sm border" ] [ Html.text "Save" ]
+        , Html.button
+            [ Attributes.class "px-4 py-2 font-medium text-center rounded-sm border"
+            ]
+            [ Html.text "Save" ]
         ]
 
 
@@ -168,16 +177,16 @@ viewLink url =
         ]
 
 
-x =
-    "https://www.fastwrx.com/collections/shift-knobs/products/cobb-6-speed-shift-knob"
+type alias SectionTitle =
+    String
 
 
 type Msg
-    = ToggleNewLinkForm String
-    | SetNewUrl String
-    | FetchTitleResponse (WebData String)
-    | UseSuggestedTitle String
-    | SetNewTitle String
+    = ToggleNewLinkForm SectionTitle
+    | SetNewUrl SectionTitle String
+    | FetchTitleResponse SectionTitle (WebData String)
+    | UseSuggestedTitle SectionTitle String
+    | SetNewTitle SectionTitle String
     | NoOp
 
 
@@ -185,7 +194,6 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ToggleNewLinkForm sectionTitle ->
-            --todo
             ( { model
                 | mods =
                     Dict.update sectionTitle
@@ -202,24 +210,79 @@ update msg model =
             , Cmd.none
             )
 
-        -- SetNewUrl url ->
-        --     -- todo debounce fetching suggested title
-        --     ( { model | newUrl = url, suggestedTitle = Loading }
-        --     , Url.fromString url
-        --         |> Maybe.map (Scraper.fetchTitle (RemoteData.fromResult >> FetchTitleResponse))
-        --         |> Maybe.withDefault Cmd.none
-        --     )
-        FetchTitleResponse res ->
-            ( model
+        SetNewUrl sectionTitle url ->
+            -- todo debounce fetching suggested title
+            ( { model
+                | mods =
+                    Dict.update sectionTitle
+                        (\v ->
+                            case v of
+                                Just section ->
+                                    Just { section | newUrl = url, suggestedTitle = Loading }
+
+                                Nothing ->
+                                    Nothing
+                        )
+                        model.mods
+              }
+            , Url.fromString url
+                |> Maybe.map
+                    (Scraper.fetchTitle
+                        (RemoteData.fromResult
+                            >> FetchTitleResponse sectionTitle
+                        )
+                    )
+                |> Maybe.withDefault
+                    (delay 0 (FetchTitleResponse sectionTitle (Success "")))
+            )
+
+        FetchTitleResponse sectionTitle res ->
+            ( { model
+                | mods =
+                    Dict.update sectionTitle
+                        (\v ->
+                            case v of
+                                Just section ->
+                                    Just { section | suggestedTitle = res }
+
+                                Nothing ->
+                                    Nothing
+                        )
+                        model.mods
+              }
             , Cmd.none
             )
 
-        UseSuggestedTitle title ->
-            update (SetNewTitle title)
-                model
+        UseSuggestedTitle sectionTitle title ->
+            update (SetNewTitle sectionTitle title)
+                { model
+                    | mods =
+                        Dict.update sectionTitle
+                            (\v ->
+                                case v of
+                                    Just section ->
+                                        Just { section | suggestedTitle = NotAsked }
 
-        SetNewTitle title ->
-            ( model
+                                    Nothing ->
+                                        Nothing
+                            )
+                            model.mods
+                }
+
+        SetNewTitle sectionTitle title ->
+            ( { model
+                | mods =
+                    Dict.update sectionTitle
+                        (\v ->
+                            case v of
+                                Just section ->
+                                    Just { section | newTitle = title }
+
+                                Nothing ->
+                                    Nothing
+                        )
+                        model.mods
+              }
             , Cmd.none
             )
 
@@ -230,3 +293,8 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
+
+delay n msg =
+    Process.sleep n
+        |> Task.perform (always msg)
