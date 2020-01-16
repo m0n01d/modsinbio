2de64d57ab990a1c1394795443d8ae2bd47a60dd
Dwight Doane <dwightdoane@me.com>
2020-01-15 20:05:01 -0500

    flags and ports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import './main.css';
 import { Elm } from './Main.elm';
 // import * as serviceWorker from './serviceWorker';
 
-Elm.Main.init({
+const flags = {
+  payload: localStorage.user ? JSON.parse(localStorage.user) : null,
+};
+
+const app = Elm.Main.init({
   node: document.getElementById('root'),
+  flags,
+});
+
+app.ports.toJs.subscribe(function fromElm({ tag, payload }) {
+  switch (tag) {
+    case 'SAVE_USER': {
+      localStorage.user = JSON.stringify(payload);
+      return;
+    }
+    default:
+      return;
+  }
 });
 
 // If you want your app to work offline and load faster, you can change
