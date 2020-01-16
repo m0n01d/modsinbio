a1280de747b084f33a51b22f5f0227ad2db0b213
Dwight Doane <dwightdoane@me.com>
2020-01-16 16:27:43 -0500

    clean up

--- src/Network/Scraper.elm
+++ src/Network/Scraper.elm
@@ -1,18 +1,15 @@
-module Network.Scraper exposing (..)
+module Network.Scraper exposing (fetchTitle)
 
 import Http
 import Url
 import Url.Builder
 
 
-type Thing
-    = Thing
-
-
+fetchTitle : (Result Http.Error String -> msg) -> Url.Url -> Cmd msg
 fetchTitle onComplete url =
     Http.get
         { url =
-            Url.Builder.absolute [ "scrape", "title" ]
+            Url.Builder.absolute [ "api", "scrape", "title" ]
                 [ Url.Builder.string "url" (Url.toString url)
                 ]
         , expect = Http.expectString onComplete
