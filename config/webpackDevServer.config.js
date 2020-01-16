a9f98723a413cb4e662224581b6dbdc9be829a68
Dwight Doane <dwightdoane@me.com>
2020-01-16 16:26:22 -0500

    fix api url

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -99,7 +99,7 @@ module.exports = function(proxy, allowedHost) {
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
       app.use(noopServiceWorkerMiddleware());
 
-      app.get('/scrape/title', require('../api/scraper').scrapeTitle);
+      app.get('/api/scrape/title', require('../api/scraper').scrapeTitle);
     },
   };
 };
