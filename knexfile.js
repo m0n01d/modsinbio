18cfb6f77d34f991acb249df786b78fa4e591158
Dwight Doane <dwightdoane@me.com>
2020-02-19 18:34:36 -0500

    get connection_url working again in dev

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,11 @@
-// @TODO
-
 const databaseName = 'postgres';
 const pg = require('pg');
 
 const connection_url =
-  process.env.DATABASE_URL ||
-  `postgres://postgres:@postgres:5432/${databaseName}`;
+  process.env.APP_ENV == 'development'
+    ? process.env.DATABASE_URL
+    : `${process.env.DATABASE_URL}?ssl=true` ||
+      `postgres://postgres:@postgres:5432/${databaseName}`;
 
 module.exports = {
   client: 'pg',
