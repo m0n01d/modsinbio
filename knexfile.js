9d5284abc18488cd54e7cbd6528b76b9e5a7082f
Dwight Doane <dwightdoane@me.com>
2020-02-21 01:15:00 -0500

    ssl var was added to env env for zeit

--- knexfile.js
+++ knexfile.js
@@ -12,10 +12,8 @@ const connection_url =
   APP_ENV == 'development'
     ? DATABASE_URL // LOCALHOST
     : NOW_GITHUB_COMMIT_REF == 'master'
-    ? PROD_DATABASE_URL // DIGITAL_OCEAN
-    : `${process.env.DATABASE_URL}?ssl=true`; // HEROKU
-
-// @TODO set up envs in heroku and zeit and .env
+    ? PROD_DATABASE_URL // DIGITAL_OCEAN -- @TODO
+    : DATABASE_URL; // HEROKU
 
 module.exports = {
   client: 'pg',
