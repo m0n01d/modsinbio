3a246fb22afd1e1b436356483609d0d25f1b0c4c
Dwight Doane <dwightdoane@me.com>
2020-04-02 01:21:09 -0400

    rm old s3

--- config/s3.js
+++
@@ -1,9 +0,0 @@
-const { S3 } = require('aws-sdk');
-
-const s3 = new S3({
-  accessKeyId: process.env.S3_ACCESS_KEY,
-  secretAccessKey: process.env.S3_SECRET_KEY,
-  region: 'us-east-1',
-});
-
-module.exports = _ => s3;
